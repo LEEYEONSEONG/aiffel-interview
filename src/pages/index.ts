4ab67ce69550f6b646e48dba04a8b2518f164c36
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-03 17:07:52 +0900

    Add : page 구조 추가

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Login } from './Login';
+export { default as ForumWrite } from './ForumWrite';
+export { default as Forum } from './Forum';
+export { default as ForumDetail } from './ForumDetail';
