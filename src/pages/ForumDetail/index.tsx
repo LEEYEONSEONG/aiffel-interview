95f884f69b5499a74425b03d3d6f830bc92e521b
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 02:24:45 +0900

    Add : detail page ui ux 구현

--- src/pages/ForumDetail/index.tsx
+++ src/pages/ForumDetail/index.tsx
@@ -1,7 +1,126 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { Button, Text } from 'components';
+import styled from 'styled-components';
+
+import { getForumDetail } from 'redux/modules/forum';
+
+import { flex } from 'styles/flex';
+import { fitImg } from 'styles/mixins';
+import { deleteIcon, likeIcon, likeIconActive } from 'assets';
+
+import { IRootState } from 'types/payloadTypes';
 
 function ForumDetail() {
-  return <div>ForumDetail</div>;
+  const { id } = useParams() as { id: string };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { forum } = useSelector((state: IRootState) => state.forum);
+  const {
+    title,
+    content,
+    tag: { name, color },
+    isLiked,
+  } = forum;
+
+  const fetchForumDetail = async (id: string) => {
+    const { data } = await axios.get(`/forum?id=${id}`);
+    dispatch(getForumDetail(data[0]));
+  };
+
+  const handleLikeToggle = async () => {
+    const { status } = await axios.patch(`/forum/${id}`, {
+      isLiked: !isLiked,
+    });
+    if (status === 200) {
+      fetchForumDetail(id);
+    }
+  };
+
+  const handleDetailDelete = async () => {
+    const confirm = window.confirm('정말로 삭제하시겠습니까?');
+
+    if (confirm) {
+      const { status } = await axios.delete(`/forum/${id}`);
+      if (status === 200) {
+        navigate('/forum');
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchForumDetail(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  return (
+    <Forum>
+      <BackButton onClick={() => navigate('/forum')}>뒤로가기</BackButton>
+      <Text.Large>{title}</Text.Large>
+      <TagBox>
+        <Tag color={color}>{name}</Tag>
+      </TagBox>
+      <Text.Medium color="#697077">{content}</Text.Medium>
+      <LikeWrap>
+        <LikeIcon onClick={() => handleLikeToggle()}>
+          <img alt="like" src={isLiked ? likeIconActive : likeIcon} />
+        </LikeIcon>
+        <DeleteIcon onClick={() => handleDetailDelete()}>
+          <img alt="delete" src={deleteIcon} />
+        </DeleteIcon>
+      </LikeWrap>
+    </Forum>
+  );
 }
 
 export default ForumDetail;
+
+const BackButton = styled(Button)`
+  background: #dde1e6;
+  border-radius: 8px;
+  width: 70px;
+  color: #ffffff;
+  height: 40px;
+  margin-bottom: 40px;
+`;
+
+const Forum = styled.div`
+  margin-top: 120px;
+  width: 700px;
+  ${flex('', '', 'column')}
+  row-gap: 14px;
+`;
+
+const TagBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  overflow: auto;
+`;
+
+const Tag = styled.div<{ color: string }>`
+  ${flex('center', 'center')};
+  background: ${({ color }) => color && color};
+  padding: 5px 6px;
+  border-radius: 9px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const LikeWrap = styled.div`
+  ${flex('flex-end', '')}
+`;
+
+const LikeIcon = styled.div`
+  ${fitImg}
+  width: 25px;
+  height: 25px;
+`;
+
+const DeleteIcon = styled(LikeIcon)`
+  margin-left: 12px;
+`;
