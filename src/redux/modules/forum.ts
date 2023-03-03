14129361e4fba2156ec0ca4e7719315db072062c
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 02:23:40 +0900

    Add : detail data redux store state 추가

--- src/redux/modules/forum.ts
+++ src/redux/modules/forum.ts
@@ -1,6 +1,7 @@
 import { IForumState, IForumData } from 'types/payloadTypes';
 
 export const GET_FORUM_LIST = 'forum/GET_FORUM_LIST';
+export const GET_FORUM_DETAIL = 'forum/GET_FORUM_DETAIL';
 
 export const getForumList = (payload: IForumData[]) => {
   return {
@@ -9,9 +10,24 @@ export const getForumList = (payload: IForumData[]) => {
   };
 };
 
+export const getForumDetail = (payload: IForumData) => ({
+  type: GET_FORUM_DETAIL,
+  payload,
+});
+
 const initialState: IForumState = {
   forumList: [],
   total: 0,
+  forum: {
+    id: 0,
+    title: '',
+    content: '',
+    isLiked: false,
+    tag: {
+      name: '',
+      color: '',
+    },
+  },
 };
 
 type ForumAction = ReturnType<typeof getForumList>;
@@ -24,6 +40,13 @@ export default function reducer(state = initialState, action: ForumAction) {
         ...state,
         forumList,
         total: forumList.length,
+      };
+
+    case GET_FORUM_DETAIL:
+      const detail = action.payload;
+      return {
+        ...state,
+        forum: detail,
       };
     default:
       return state;
