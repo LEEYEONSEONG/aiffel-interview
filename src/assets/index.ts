155f25d4f856eb594768ebf26895aba44c326a52
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 02:22:43 +0900

    Add : detail page 필요한 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,6 @@ export { default as aiffelLogo } from 'assets/ic-aiffel-logo.png';
 export { default as profile } from 'assets/ic-profile.png';
 export { default as searchIconActive } from 'assets/ic-active-search.png';
 export { default as searchIcon } from 'assets/ic-inActive-search.png';
+export { default as likeIconActive } from 'assets/ic-active-like.png';
+export { default as likeIcon } from 'assets/ic-like.png';
+export { default as deleteIcon } from 'assets/ic-delete.png';
