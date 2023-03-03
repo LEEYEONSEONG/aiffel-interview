2258373fb17213b6c91b59c8175e5ab4318271d8
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 00:16:36 +0900

    Add : 공통 text componenet 추가

---
+++ src/components/Text/components/TextMedium.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+import { fontSize } from 'styles/mixins';
+interface ITextMedium {
+  children: ReactNode;
+  weight?: number;
+  color?: string;
+}
+
+function TextMedium(props: ITextMedium) {
+  const { children, weight = 500, color = '#191919', ...restProps } = props;
+
+  return (
+    <Text color={color} weight={weight} {...restProps}>
+      {children}
+    </Text>
+  );
+}
+
+export default TextMedium;
+
+interface ITextStyle {
+  weight: number;
+  color: string;
+}
+
+const Text = styled.span<ITextStyle>`
+  font-size: ${fontSize.medium};
+  font-weight: ${(props) => props.weight};
+  color: ${(props) => props.color};
+`;
---
+++ src/components/Text/components/TextRegular.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+import { fontSize } from 'styles/mixins';
+interface ITextRegular {
+  children: ReactNode;
+  weight?: number;
+  color?: string;
+}
+
+function TextRegular(props: ITextRegular) {
+  const { children, weight = 400, color = '#191919', ...restProps } = props;
+
+  return (
+    <Text color={color} weight={weight} {...restProps}>
+      {children}
+    </Text>
+  );
+}
+
+export default TextRegular;
+
+interface ITextStyle {
+  weight: number;
+  color: string;
+}
+
+const Text = styled.span<ITextStyle>`
+  font-size: ${fontSize.regular};
+  font-weight: ${(props) => props.weight};
+  color: ${(props) => props.color};
+`;
