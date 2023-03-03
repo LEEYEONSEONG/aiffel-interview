7b309645ebb5484b1ec6c8921084a2e3c4fde85f
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-03 17:08:23 +0900

    Add : Route 및 private, check header 구현

---
+++ src/Routes.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  Outlet,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { Login, Forum, ForumWrite, ForumDetail } from 'pages';
+import { Layout, Header } from 'components';
+
+import { IRootState } from 'types/payloadTypes';
+
+const PrivateRoute = () => {
+  const {
+    user: { username },
+  } = useSelector((state: IRootState) => state.auth);
+
+  return username ? <Outlet /> : <Navigate to="/" />;
+};
+
+const CheckHeader = () => {
+  const {
+    user: { username },
+  } = useSelector((state: IRootState) => state.auth);
+
+  return username ? <Header /> : <div />;
+};
+
+const ConditionalHeader = CheckHeader;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ConditionalHeader />
+      <Layout>
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/write" element={<ForumWrite />} />
+            <Route path="/detail" element={<ForumDetail />} />
+          </Route>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
