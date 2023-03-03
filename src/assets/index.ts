7b21c10030f7d68a52b9c52a6e2b19177f77ea2c
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 00:13:19 +0900

    Add : search bar images 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as aiffelLogo } from 'assets/ic-aiffel-logo.png';
 export { default as profile } from 'assets/ic-profile.png';
+export { default as searchIconActive } from 'assets/ic-active-search.png';
+export { default as searchIcon } from 'assets/ic-inActive-search.png';
