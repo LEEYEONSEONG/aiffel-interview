016b1be1dd1785a1cafe691e54592dadcae8a536
LEEYEONSEONG <dustjd1535@gmail.com>
2023-03-04 13:46:14 +0900

    Add : back icon component 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,5 @@ export { default as searchIcon } from 'assets/ic-inActive-search.png';
 export { default as likeIconActive } from 'assets/ic-active-like.png';
 export { default as likeIcon } from 'assets/ic-like.png';
 export { default as deleteIcon } from 'assets/ic-delete.png';
+export { default as selectIcon } from 'assets/ic-select.png';
+export { default as backArrow } from 'assets/ic-back-arrow.png';
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as SearchBar } from './SearchBar';
 export { default as Text } from './Text';
+export { default as BackIcon } from './BackIcon';
